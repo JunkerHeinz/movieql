49400c9eb06fe58b6be9d09087463145718aa521
bluemaxg <ihavecontrol@ucomp.co.kr>
2020-09-16 10:50:21 +0900

    delete mutation 까지

--- graphql/db.js
+++ graphql/db.js
@@ -1,53 +1,52 @@
-const movies = [
+let movies = [
     {
-        id: 0, 
-        name: "star wars - The New One",
-        score: 1
+      id: 0,
+      name: "Star Wars - The new one",
+      score: 1
     },
     {
-        id: 1, 
-        name: "Avengers - yap yap",
-        score: 8
+      id: 1,
+      name: "Avengers - The new one",
+      score: 8
     },
     {
-        id: 2, 
-        name: "The God Father I",
-        score: 99
+      id: 2,
+      name: "The Godfather I",
+      score: 99
     },
     {
-        id: 3, 
-        name: "Logan",
-        score: 2
+      id: 3,
+      name: "Logan",
+      score: 2
     }
-];
+  ];
 
 export const getMovies = () => movies;
 
-export const getById = id => { 
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
 };
 
-export const deleteMovie = (id) => { 
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
-    if(movies.length > cleanedMovies.length) {
-        movie = cleanedMovies;
-        return true;
-    } else { 
-        return false; 
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+      movies = cleanedMovies;
+      return true;
+    } else {
+      return false;
     }
 };
 
 export const addMovie = (name, score) => {
     const newMovie = {
-        id: `${movies.length + 1}`,
-        name, 
-        score
-    }; 
+      id: `${movies.length + 1}`,
+      name,
+      score
+    };
     movies.push(newMovie);
     return newMovie;
-};
-
+  };
 
 // export const people = [ 
 //     {
